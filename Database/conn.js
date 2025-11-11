16f6a8754dae646fe66bf4f31dd980c4877eabc9
Muhammad Faizan Aziz <165632955+Faizan-Aziz@users.noreply.github.com>
2025-11-11 20:02:23 +0500

    Update conn.js

--- Database/conn.js
+++ Database/conn.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import { startPublisher } from "../scheduler/publisher.js";
 
 dotenv.config();
 
 const databaseconnection = mongoose.connect(process.env.MONGODB_URI )
 .then(() => {
     console.log("Database connected successfully");
+     startPublisher();
 }).catch((err) => {
     console.log("Database connection error:", err);
 });
